c6b4383f4f17e53ca268a663ca2b7e666ad950c1
Aldair <djaldair14@gmail.com>
2023-08-21 17:03:03 -0500

    project added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,7 @@ import projImg5 from "../images/img/project-img5.png";
 import projImg6 from "../images/img/project-img6.png";
 import projImg7 from "../images/img/project-img7.png";
 import projImg8 from "../images/img/project-img8.png";
+import projImg9 from "../images/img/project-img9.png";
 import projImg10 from "../images/img/project-img10.png";
 import projImg11 from "../images/img/project-img11.png";
 import projImg12 from "../images/img/project-img12.png";
@@ -67,6 +68,12 @@ export const Projects = () => {
       description: "Desing & Development",
       imgUrl: projImg8,
       url: "https://moviesapparb.netlify.app/",
+    },
+    {
+      title: "Ecommerce",
+      description: "Desing & Development",
+      imgUrl: projImg9,
+      url: "https://ecommerce-arb.netlify.app/",
     },
     {
       title: "Tasks app",
