126dca9b259c4db33845f1e43fa02974c8d85643
Aldair <71675300+aldair1630@users.noreply.github.com>
2025-01-30 22:21:52 +0000

    ajuste de padding en el pie de página y desactivación temporal del formulario de Mailchimp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import logo from "../images/logo.png";
 import navIcon1 from "../images/img/nav-icon1.svg";
 import navIcon2 from "../images/img/nav-icon2.svg";
@@ -10,7 +9,6 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          {/* <MailchimpForm /> */}
           <Col sm={6}>
             <img src={logo} alt={logo} className="logo-arb" />
           </Col>
